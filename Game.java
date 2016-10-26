d5b71888c3771ab16dd198ee1c3b6e5f50645043
Rocelle <annecadavos@yahoo.com>
2016-10-26 22:40:43 +0800

    Update Game.java

--- Game.java
+++ Game.java
@@ -8,14 +8,19 @@ import java.awt.event.KeyListener;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+/*
+ * created by rocelle cadavos
+    10/21/2016
+ */
 
 @SuppressWarnings("serial")
 public class Game extends JPanel {
-
+    //score counter for player 1 and player 2
     public int score1;
     public int score2;
-
+    //we created an instance  of a ball for the game
     Ball ball = new Ball(this);
+    //these are the racquets for both players, also giving them their positions
     Racquet racquet1 = new Racquet(this, 150, 330);
     Racquet racquet2 = new Racquet(this, 150, 50);
 
@@ -38,11 +43,13 @@ public class Game extends JPanel {
             }
 
             @Override
+            //first player  has controls LEFT and RIGHT 
             public void keyPressed(KeyEvent e) {
                 if (e.getKeyCode() == KeyEvent.VK_LEFT)
                     racquet1.moveLeft();
                 if (e.getKeyCode() == KeyEvent.VK_RIGHT)
                     racquet1.moveRight();
+                //second player has controls A and S
                 if (e.getKeyCode() == KeyEvent.VK_A)
                     racquet2.moveLeft();
                 if (e.getKeyCode() == KeyEvent.VK_S)
@@ -73,7 +80,7 @@ public class Game extends JPanel {
         ball.paint(g2d);
         racquet1.paint(g2d);
         racquet2.paint(g2d);
-
+        //colors of the score, their font and their position
         g2d.setColor(Color.BLUE);
         g2d.setFont(new Font("Helvetica", Font.BOLD, 30));
         g2d.drawString(String.valueOf(score1), 10, 30);
@@ -81,7 +88,7 @@ public class Game extends JPanel {
         g2d.setFont(new Font("Helvetica", Font.BOLD, 30));
         g2d.drawString(String.valueOf(score2), 10, 330);
     }
-
+    //game over  pop up dialog
     public void gameOver() throws InterruptedException{
         if(JOptionPane.showConfirmDialog(this, "Game over! Play again?",
                 "Game Over", JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) {
@@ -91,7 +98,7 @@ public class Game extends JPanel {
             System.exit(ABORT);
         }
     }
-
+    //end game dialog that shows who won and if they want to play again
     public void displayScore() {
         JOptionPane.showMessageDialog(this, "Player 1:" + score1 +  "\n Player 2:" + score2,
                 "Game Over", JOptionPane.YES_NO_OPTION);
@@ -103,17 +110,22 @@ public class Game extends JPanel {
             repaint();
             Thread.sleep(10);
             if(score1 == 3 || score2 == 3) {
+                //if someone has scored 3, game over
                 gameOver();
             }
         }
     }
 
     public static void main(String[] args) throws InterruptedException {
+        //created the main frame for our game
         JFrame frame = new JFrame("PING PONG");
         Game game = new Game();
+        //with the following specifications:
         frame.add(game);
         frame.setSize(300, 400);
         frame.setVisible(true);
+        frame.setLocation(600, 300);
+        frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         game.play();
